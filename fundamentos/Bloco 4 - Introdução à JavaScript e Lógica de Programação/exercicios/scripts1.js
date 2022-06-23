4a996010214dee1ee39f6f7f29d04f4fa19f2dad
Pedro <pedrosteinmuller10105@hotmail.com>
2022-06-23 18:05:07 -0300

    desafio logica programacao

---
+++ fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/exercicios/scripts1.js
@@ -0,0 +1,40 @@
+// Desafios/Exercicios logica programacao dia 3 -------------------------
+
+// 1. Faça um algoritmo que calcule a soma de 35 a 105 usando a estrutura “for” e retorne no formato:
+// A soma de 35 a 105 é: X.
+// 2. Crie um algoritmo que conte quantos números do intervalo entre 15 e 155 são divisíveis por 3.
+// Caso a quantidade de números impares seja igual ou maior que 50, exiba uma mensagem secreta a sua escolha.
+// 3. Crie um algoritmo que simula o jogo “pedra, papel e tesoura” levando em consideração  que são apenas duas pessoas jogando e imprima o resultado no formato:
+// “Jogador 1 vence” ou “Empate” ou “Jogador 2 vence”.
+// 4. Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
+// Imprima no console seguindo o exemplo: “A pessoa é maior de idade” ou “A pessoa é menor de idade”.
+// Bônus: Crie a condição utilizando operador ternário.
+// 5. Crie um algoritmo que recebe a idade de Ademar, Adriana e Julia e imprima quem é a mais nova no formato:
+// “Pessoa” é a mais nova. (edited) 
+
+// let soma = 0
+
+// for(index = 35; index <= 105; index+=1)
+// {
+//     soma += index
+// }
+// console.log (`A soma de 35 a 105 é: ${soma}`)
+
+
+// let soma = []
+
+// for(index = 15; index <= 155; index+=1)
+// {
+//     if(index % 3 == 0)
+//     {soma.push(index)}
+
+// }
+
+// for (index = 0; index < soma.length; index +=1)
+// {
+//    if (soma[index] % 2 != 0)
+// }
+// console.log (`A quantidade de números impares seja : ${soma.length}`)
+
+// console.log(soma)
+
