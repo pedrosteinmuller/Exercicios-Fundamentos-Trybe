a687c721c7c27e5502d260185f727c81df7a84eb
Pedro <pedrosteinmuller10105@hotmail.com>
2022-06-24 19:28:30 -0300

    resolvendo exercicios do dia 5 do bloco 4

---
+++ fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/exercicios/scripts2.js
@@ -0,0 +1,36 @@
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: {
+//         golden: 2,
+//         silver: 3,
+//     },
+//     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+// }
+
+// // 
+// // console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+//   console.log(cars[index]);
+// }
+
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let index in car) {
+//     console.log(index, car[index]);
+//   }
+
+//   let food = ['hamburguer', 'bife', 'acarajé'];
+// for (let value of food) {
+//   console.log(value);
+// };
+
