4a794127cd556fade115898232b51f6695e377c5
Pedro <pedrosteinmuller10105@hotmail.com>
2022-07-25 21:33:14 -0300

    resolucao de de alguns exercicios do bloco 7 e 8 que esqueci de dar commit antes

---
+++ fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
@@ -0,0 +1,119 @@
+// // const arr = ['Ana', 'Pedro', 'Giovana'];
+
+// // arr.filter((element) => console.log(element))
+
+// const pessoas = ['pessoaA', 'pessoaB', 'pessoaC', 'pessoaD', 'pessoaE'];
+
+// const novasPessoas = pessoas.map((element) => {
+//   const registro = {};
+//   registro.nome = element;
+//   registro.empresa = 'Trybe';
+//   return registro;
+// });
+
+// console.log(novasPessoas
+  
+//   );
+
+//   const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+// const prices = [2.99, 3.99, 1.5, 2];
+
+// const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+//   { [product]: listPrices[index] }
+// ));
+
+// const listProducts = updateProducts(products, prices);
+// console.log(listProducts);
+
+
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: '59' },
+      { name: 'Português', nota: '80' },
+      { name: 'Química', nota: '78' },
+      { name: 'Biologia', nota: '92' },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '76' },
+      { name: 'Português', nota: '90' },
+      { name: 'Química', nota: '70' },
+      { name: 'Biologia', nota: '80' },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '91' },
+      { name: 'Português', nota: '85' },
+      { name: 'Química', nota: '92' },
+      { name: 'Biologia', nota: '90' },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '70' },
+      { name: 'Português', nota: '70' },
+      { name: 'Química', nota: '60' },
+      { name: 'Biologia', nota: '50' },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '80' },
+      { name: 'Português', nota: '82' },
+      { name: 'Química', nota: '79' },
+      { name: 'Biologia', nota: '75' },
+    ],
+  },
+];
+
+// const allNameStudents = estudantes.filter((estudante) => (
+//   estudante.turno === 'Manhã')).map((estudante) => `${estudante.nome} ${estudante.sobrenome}`);
+
+// console.log(allNameStudents);
+
+const reportStatus = (name, students) => {
+  const studentInfo = students.find((student) => student.nome === name);
+  return studentInfo.materias.map((materia) => (
+    `${materia.name} ${(materia.nota >= 60) ? 'Aprovado' : 'Reprovado'}`
+  ));
+};
+
+console.log(reportStatus('Wilson', estudantes));
+
+
